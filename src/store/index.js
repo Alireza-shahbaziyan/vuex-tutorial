4b321fc8ebf4c62d9e089bc24b92035a38cb499f
alireza-shahbaziyan <alishahbazi0831@gmail.com>
2022-09-08 15:48:46 +0430

    frist

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,40 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    counter: 0,
+    colorCode:"red"
   },
   getters: {
+    counterSquared(state){
+      return Math.pow(state.counter,2)
+    }
   },
   mutations: {
+    addCounter(state, number) {
+      state.counter += number
+    },
+    reduceCounter(state,number) {
+      state.counter -= number
+    },
+    changeColor(state,value){
+      state.colorCode = value
+    }
   },
   actions: {
+    addCounter() {
+      axios('https://www.random.org/integers/?num=1&min=1&mhttps://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=newax=6&col=1&base=10&format=plain&rnd=new')
+        .then(res => {
+          this.commit('addCounter', res.data)
+        })
+    },
+    reduceCounter() {
+      axios('https://www.random.org/integers/?num=1&min=1&mhttps://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=newax=6&col=1&base=10&format=plain&rnd=new')
+        .then(res => {
+          this.commit('reduceCounter', res.data)
+        })
+    }
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div class="h1" :style="{ color: $store.state.colorCode }">
+      {{ $store.state.counter }}
+    </div>
+    <div>
+      {{ $store.state.counter }} <sup>2</sup> ={{
+        $store.getters.counterSquared
+      }}
+    </div>
+    <div>
+      <button
+        @click="$store.dispatch('reduceCounter')"
+        class="btn btn-lg btn-outline-dark mx-2"
+      >
+        -
+      </button>
+      <button
+        @click="$store.dispatch('addCounter')"
+        class="btn btn-lg btn-outline-dark mx-2"
+      >
+        +
+      </button>
+    </div>
+    <div class="my-4">
+      <input type="text" placeholder="Color code" v-model="colorCode" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  computed: {
+    colorCode: {
+      get() {
+        this.$store.state.colorCode;
+        console.log('get');
+      },
+      set(value) {
+        console.log('send');
+        this.$store.commit("changeColor", value);
+      },
+    },
+  },
+};
 </script>
